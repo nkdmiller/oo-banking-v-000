7ce75a216afe8251b889a33c1bad6853dd2008f5
Nate Miller <natekmiller96@gmail.com>
2018-07-03 21:45:53 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,32 @@
 class Transfer
-  # your code here
+  @@all = []
+  @@executed = []
+  attr_accessor :sender, :reciever, :transfer_amount
+  def initialize(sender, reciever, transfer_amount)
+    @sender = sender
+    @reciever = reciever
+    @transfer_amount = transfer_amount
+    @status = "pending"
+    @@all << self
+  end
+  def valid?
+    if @sender.valid? == true @@ @reciever.valid? == true
+      return true
+    else
+      return false
+    end
+  end
+  def execute_transfer
+    if valid? == true
+      @sender.balance -= @amount
+      @reciever.balance += @amount
+      @@executed << self
+    end
+  end
+  def reverse_transfer(transfer)
+    @sender.balance -= @amount
+    @reciever.balance += @amount
+    @@executed.delete(self)
+    @@all.delete(self)
+  end
 end
