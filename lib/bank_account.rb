69bb3932b6b4b92eeff1c277de4684ced876c9b7
Nate Miller <natekmiller96@gmail.com>
2018-07-03 21:32:49 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,23 @@
 class BankAccount
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+  def deposit
 
+  end
+  def display_balance
+
+  end
+  def valid?
+    if @status == "open" && @balance > 0
+      return true
+    else
+      return false
+    end
+  end
+  def close_account
+    @status = "closed"
+  end
 end
